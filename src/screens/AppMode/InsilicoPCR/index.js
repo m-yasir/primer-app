ece491571a1d44c9c4f4ceb4f0f14672f3df8394
Yasir Nadeem <m-yasir.dev@protonmail.com>
2020-07-22 07:31:23 +0500

    Complete InsilicoPCR component implementation. Remove unneeded commented code.

--- src/screens/AppMode/InsilicoPCR/index.js
+++ src/screens/AppMode/InsilicoPCR/index.js
@@ -1,175 +1,260 @@
 import React, { useState, useRef } from "react";
 import {
-  StyleSheet,
-  Alert,
-  Keyboard,
-  ScrollView,
-  KeyboardAvoidingView,
-  Dimensions,
+	StyleSheet,
+	Alert,
+	Keyboard,
+	ScrollView,
+	KeyboardAvoidingView,
+	Dimensions
 } from "react-native";
 import { Text, Layout, Input, Button } from "react-native-ui-kitten";
 import { WrapComponentWithKittenProvider } from "../../../components/utils/theming";
 import { useNavigation } from "react-navigation-hooks";
-import { useMount } from "../../../components/utils/appUtil";
 
 const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-  container: {
-    // margin: 20,
-    overflow: "scroll",
-    backgroundColor: "#fff",
-    minWidth: width,
-    minHeight: height,
-  },
-  fontSize20: {
-    fontSize: 20
-  },
-  fieldsContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    margin: 10,
-  },
-  mh5: {
-    marginHorizontal: 5
-  },
-  initialInputField: {
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  resultsContainer: {
-    display: "flex",
-    flexDirection: "column",
-    margin: 20,
-  },
-  sequenceCharacter: {
-    marginHorizontal: 2
-  },
-  textHeadingLayout: {
-    backgroundColor: "#319ede",
-    marginTop: 10,
-    marginBottom: 10,
-    minWidth: "55%",
-    maxWidth: "70%",
-    padding: 5,
-    paddingRight: 0,
-  },
-  textHeading: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 18,
-  },
+	container: {
+		// margin: 20,
+		overflow: "scroll",
+		backgroundColor: "#fff",
+		minWidth: width,
+		minHeight: height
+	},
+	fontSize20: {
+		fontSize: 20
+	},
+	fieldsContainer: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		margin: 10
+	},
+	mh5: {
+		marginHorizontal: 5
+	},
+	initialInputField: {
+		marginLeft: 10,
+		marginRight: 10
+	},
+	resultsContainer: {
+		display: "flex",
+		flexDirection: "column",
+		margin: 20
+	},
+	sequenceCharacter: {
+		paddingHorizontal: 2,
+		paddingVertical: 2
+	},
+	sequenceContainer: {
+		alignItems: "center",
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "flex-start",
+		marginHorizontal: 5,
+		marginVertical: 10
+	},
+	textHeadingLayout: {
+		backgroundColor: "#319ede",
+		marginTop: 10,
+		marginBottom: 10,
+		minWidth: "55%",
+		maxWidth: "70%",
+		padding: 5,
+		paddingRight: 0
+	},
+	textHeading: {
+		color: "white",
+		fontWeight: "bold",
+		fontSize: 18
+	}
 });
 
 const InsilicoPCR = () => {
-  // Nav-hook
-  const navigation = useNavigation();
-  // Non-states
-  const isForward = useRef(true);
-  const dnaSequence = useRef(navigation.getParam("DNA"));
-  // Component States
-  const [userFP, setUserFP] = useState("");
-  const [userRP, setUserRP] = useState("");
-  const [sequences, setSequences] = useState(null);
+	// Functions needed to be initialized earlier and in order due to usage order.
+	const getSequenceCharacter = (char) => {
+		switch (char) {
+			case "5'":
+				return "3'";
+			case "3'":
+				return "5'";
+			case "A":
+				return "T";
+			case "T":
+				return "A";
+			case "C":
+				return "G";
+			case "G":
+				return "C";
+		}
+		return char;
+	};
 
-  const getCharCountFromString = (string = "", char) => {
-    return string.split(char).length - 1;
-  };
+	const calculateSequencing = (seq) =>
+		seq
+			.split("")
+			.map((char) => getSequenceCharacter(char))
+			.join("");
 
-  const reverseString = (str) => str.split("").reduce((a, b) => b + a) + "";
+	const reverseString = (str) => str.split("").reduce((a, b) => b + a) + "";
+	// Nav-hook
+	const navigation = useNavigation();
+	// Non-states
+	const dnaSequence = useRef(
+		(() => {
+			const original = navigation.getParam("DNA");
+			const forward = calculateSequencing(original);
+			return {
+				original,
+				forward,
+				reverse: reverseString(forward)
+			};
+		})()
+	);
+	// Component States
+	const [userFP, setUserFP] = useState("");
+	const [userRP, setUserRP] = useState("");
+	const [sequences, setSequences] = useState(null);
 
-  useMount(() => {});
+	// const getCharCountFromString = (string = "", char) => {
+	// 	return string.split(char).length - 1;
+	// };
 
-  const getSequenceCharacter = (char) => {
-    switch (char) {
-      case "5'":
-        return "3'";
-      case "3'":
-        return "5'";
-      case "A":
-        return "T";
-      case "T":
-        return "A";
-      case "C":
-        return "G";
-      case "G":
-        return "C";
-      default:
-        return char;
-    }
-  };
+	const getSequencingColorCharacters = (sequence, isReverse = false) => {
+		return (isReverse ? reverseString(sequence) : sequence)
+			.split("")
+			.map((char) => getSequenceCharacter(char))
+			.map(mapCharacterWithColor);
+	};
 
-  const calculateSequencing = (sequence) => {
-    return sequence
-      .split("")
-      .map((char) => getSequenceCharacter(char))
-      .map(mapCharacterWithColor);
-      // .join("");
-  };
+	const highlightText = (start, end) => (text, idx) => {
+		return React.cloneElement(text, {
+			style: text.props.style.concat(
+				idx >= start && idx <= end ? { backgroundColor: "yellow" } : {}
+			)
+		});
+	};
 
-  const getSequenceCharColor = (char) => {
-    switch (char) {
-      case "T":
-        return "#d20000";
-      case "A":
-        return "#0200ad";
-      case "G":
-        return "#4a9026";
-      default:
-        return "#000";
-    }
-  };
-  
-  const mapCharacterWithColor = (char, idx) => (
-		<Text key={idx} style={[styles.fontSize20, styles.sequenceCharacter, { color: getSequenceCharColor(char) }]}>{char}</Text>
-  );
+	const getSequenceCharColor = (char) => {
+		switch (char) {
+			case "T":
+				return "#d20000";
+			case "A":
+				return "#0200ad";
+			case "G":
+				return "#4a9026";
+			default:
+				return "#000";
+		}
+	};
 
-  const setMainSequences = () => {
-    const _sequences = {
-      // fp: userFP,
-      // rp: userRP,
-      fp: "5",
-      rp: "3"
-    };
-    setSequences(_sequences);
-  };
+	/**
+	 *
+	 * @param {string} sequence
+	 * @param {string} subsequence
+	 */
+	const getBpIndices = (sequence, subsequence) => {
+		const startIdx = sequence.search(subsequence);
+		const endIdx = startIdx !== -1 ? startIdx + subsequence.length - 1 : -1;
+		return [startIdx, endIdx];
+	};
 
-  return (
+	const mapCharacterWithColor = (char, idx) => (
+		<Text
+			key={idx}
+			style={[
+				styles.fontSize20,
+				styles.sequenceCharacter,
+				{ color: getSequenceCharColor(char) }
+			]}
+		>
+			{char}
+		</Text>
+	);
+
+	const getHighlightedSequence = (sequence, start, end) => {
+		let notFound = false;
+		if (start === -1 || end === -1) {
+			notFound = true;
+			Alert.alert("Forward or reverse primer not found");
+		}
+		return notFound
+			? sequence.split("").map(mapCharacterWithColor)
+			: sequence
+					.split("")
+					.map(mapCharacterWithColor)
+					.map(highlightText(start, end));
+	};
+
+	// TODO: For hint use ALERT :D
+	const setMainSequences = () => {
+		if (!userFP || !userRP) {
+			Alert.alert(
+				"Information",
+				"Please enter both reverse and forward primer!"
+			);
+			return;
+		}
+		const _sequences = {
+			fpIndices: getBpIndices(
+				dnaSequence.current.forward,
+				calculateSequencing(userFP)
+			),
+			rpIndices: getBpIndices(
+				dnaSequence.current.forward,
+				reverseString(calculateSequencing(userRP))
+			)
+		};
+		setSequences(_sequences);
+	};
+
+	return (
 		<KeyboardAvoidingView>
 			<ScrollView style={styles.container}>
 				<Layout style={styles.mh5}>
 					<Layout>
-						<Layout
-							style={{
-								alignItems: "center",
-								display: "flex",
-								flexDirection: "row",
-								justifyContent: "flex-start",
-								marginHorizontal: 5,
-								marginVertical: 10
-							}}
-						>
-							<Text style={[styles.fontSize20, styles.sequenceCharacter]}>5'</Text>
-							{calculateSequencing(dnaSequence.current)}
-							<Text style={[styles.fontSize20, styles.sequenceCharacter]}>3'</Text>
-						</Layout>
-						<Layout
-							style={{
-								alignItems: "center",
-								display: "flex",
-								flexDirection: "row",
-								justifyContent: "flex-start",
-								marginHorizontal: 5,
-								marginVertical: 10
-							}}
-						>
-							<Text style={[styles.fontSize20, styles.sequenceCharacter]}>3'</Text>
-							{calculateSequencing(
-								reverseString(dnaSequence.current)
+						<Layout style={styles.sequenceContainer}>
+							<Text
+								style={[
+									styles.fontSize20,
+									styles.sequenceCharacter
+								]}
+							>
+								5'
+							</Text>
+							{getSequencingColorCharacters(
+								dnaSequence.current.forward
 							)}
-							<Text style={[styles.fontSize20, styles.sequenceCharacter]}>5'</Text>
+							<Text
+								style={[
+									styles.fontSize20,
+									styles.sequenceCharacter
+								]}
+							>
+								3'
+							</Text>
+						</Layout>
+						<Layout style={styles.sequenceContainer}>
+							<Text
+								style={[
+									styles.fontSize20,
+									styles.sequenceCharacter
+								]}
+							>
+								3'
+							</Text>
+							{getSequencingColorCharacters(
+								dnaSequence.current.reverse
+							)}
+							<Text
+								style={[
+									styles.fontSize20,
+									styles.sequenceCharacter
+								]}
+							>
+								5'
+							</Text>
 						</Layout>
 					</Layout>
 				</Layout>
@@ -212,152 +297,49 @@ const InsilicoPCR = () => {
 				{sequences && (
 					<Layout style={styles.mh5}>
 						<Layout>
-							<Layout
-								style={{
-									alignItems: "center",
-									display: "flex",
-									flexDirection: "row",
-									justifyContent: "flex-start",
-									marginHorizontal: 5,
-									marginVertical: 10
-								}}
-							>
+							<Layout style={styles.sequenceContainer}>
 								<Text style={styles.fontSize20}>5'</Text>
-								{calculateSequencing(dnaSequence.current)}
+								{getHighlightedSequence(
+									dnaSequence.current.forward,
+									sequences.fpIndices[0],
+									sequences.fpIndices[1]
+								)}
 								<Text style={styles.fontSize20}>3'</Text>
 							</Layout>
-							<Layout
-								style={{
-									alignItems: "center",
-									display: "flex",
-									flexDirection: "row",
-									justifyContent: "flex-start",
-									marginHorizontal: 5,
-									marginVertical: 10
-								}}
-							>
+							<Layout style={styles.sequenceContainer}>
 								<Text style={styles.fontSize20}>3'</Text>
-								{calculateSequencing(
-									reverseString(dnaSequence.current)
+								{getHighlightedSequence(
+									dnaSequence.current.reverse,
+									sequences.rpIndices[0],
+									sequences.rpIndices[1]
 								)}
 								<Text style={styles.fontSize20}>5'</Text>
 							</Layout>
+							{/* Product Size */}
+							<Layout>
+								<Text>
+									Product Size:{" "}
+									{sequences.fpIndices[0] === -1 ||
+									sequences.rpIndices[1] === -1
+										? "Not available"
+										: `${
+												sequences.rpIndices[1] -
+												sequences.fpIndices[0]
+										  } bp`}
+								</Text>
+							</Layout>
 						</Layout>
-						{/* <Layout style={styles.textHeadingLayout}>
-							<Text
-								style={{
-									...styles.textHeading,
-									// width: "70%",
-									textAlign: "center"
-								}}
-							>
-								Forward Primer:
-							</Text>
-						</Layout>
-						<Layout style={styles.fieldsContainer}>
-							<Input
-								// onChangeText={handleInputChange("start")}
-								disabled
-								style={{
-									minWidth: 200
-								}}
-							/>
-						</Layout>
-						<Layout style={styles.textHeadingLayout}>
-							<Text
-								style={[
-									styles.textHeading,
-									{ textAlign: "center" }
-								]}
-							>
-								Reverse Primer:
-							</Text>
-						</Layout>
-						<Layout style={styles.fieldsContainer}>
-							<Input
-								// onChangeText={handleInputChange("start")}
-								disabled
-								style={{
-									minWidth: 200
-								}}
-							/>
-						</Layout> */}
 					</Layout>
 				)}
-				{/* <Layout style={styles.textHeadingLayout}>
-          <Text style={styles.textHeading}>Nucleotide Sequence:</Text>
-        </Layout>
-        <Text style={{ textAlign: "center" }}>{` ${dnaSequence}`}</Text>
-        <Text style={{ marginTop: 5, marginBottom: 5 }}>
-          <Text style={{ fontWeight: "bold" }}>Total Nucleotide:</Text>
-          {` ${dnaSequence.length}`}
-        </Text>
-        <Layout style={styles.fieldsContainer}>
-          <Input
-            keyboardType="numeric"
-            value={formValues.start}
-            disabled
-            onChangeText={handleInputChange("start")}
-            placeholder="Enter DNA Start"
-          />
-          <Input
-            keyboardType="numeric"
-            value={formValues.end}
-            onChangeText={handleInputChange("end")}
-            placeholder="Enter DNA End"
-          />
-        </Layout>
-        <Button
-          appearance="outline"
-          status="primary"
-          onPress={calculateValues(+formValues.start, +formValues.end)}
-        >
-          Calculate
-        </Button>
-        <Layout style={styles.textHeadingLayout}>
-          <Text
-            style={{ ...styles.textHeading, width: "70%", textAlign: "center" }}
-          >
-            Forward Primer:
-          </Text>
-        </Layout>
-        <Layout style={styles.fieldsContainer}>
-          <Input
-            value={formValues.start}
-            disabled
-            style={{
-              minWidth: 200,
-            }}
-            // onChangeText={handleInputChange("start")}
-            value={forwardPrimer}
-          />
-        </Layout>
-        <Layout style={styles.textHeadingLayout}>
-          <Text
-            style={[styles.textHeading, { width: "70%", textAlign: "center" }]}
-          >
-            Reverse Primer:
-          </Text>
-        </Layout>
-        <Layout style={styles.fieldsContainer}>
-          <Input
-            value={formValues.start}
-            disabled
-            style={{
-              minWidth: 200,
-            }}
-            value={reversePrimer}
-          />
-        </Layout> */}
 			</ScrollView>
 		</KeyboardAvoidingView>
-  );
+	);
 };
 
 const InsilicoPCRContainer = WrapComponentWithKittenProvider(InsilicoPCR);
 
 InsilicoPCRContainer.navigationOptions = ({ navigation }) => ({
-  title: navigation.getParam("headerTitle"),
+	title: navigation.getParam("headerTitle")
 });
 
 export default InsilicoPCRContainer;
